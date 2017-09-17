bd35546186a16fe18ea0f80de5c36f90058f0f5b
shresthaShailav <31478218+shresthaShailav@users.noreply.github.com>
2017-09-17 17:49:16 +0530

    Update GitClass.py

--- GitClass.py
+++ GitClass.py
@@ -1,21 +1,19 @@
 from github import Github
+import unittest
 import sys
 	
-
 class GitOperations:
-	""" Some basic git operations applicable to the current project"""
+	
 	def __init__(self, name, password):
 	    self.git_object = Github(name, password)
             self.user = self.git_object.get_user()
 
 	def create_repo(self, repo_name):
-		
             """ Creates a new repository an returns a repository"""
             repo = self.user.create_repo(repo_name)
-	    return repo				
-		
-                
+            return repo
+
         def delete_repo(self, repo_name):
             """ Deletes the repo_name if it exists in the repository"""
             repo = self.user.get_repo(repo_name)
-	    repo.delete()
+            repo.delete()	    
